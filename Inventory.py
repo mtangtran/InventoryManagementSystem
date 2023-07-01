bb2e930f0d96d0c30728dee2cb2c548eed172048
Michael <44344017+mtangtran@users.noreply.github.com>
2023-07-01 11:33:00 -0400

    Initial commit

---
+++ Inventory.py
@@ -0,0 +1,32 @@
+class Inventory:
+
+    def __init__(self, name):
+        self.inventory = {}
+        self.storageSpaceName = name
+        self.item_id = 0
+
+    def addInventory(self, item, quantity):
+
+        if item in self.item_id:
+            self.inventory[item] = quantity
+
+        else:
+            self.inventory[item] = quantity
+
+    def deleteInventory(self, item):
+        if item in self.inventory:
+            self.inventory.pop(item)
+
+    def getInventoryItem(self, item):
+        if item in self.inventory:
+            return self.inventory[item]
+        else:
+            return None
+
+    def updateInventory(self, item, quantity):
+        if item in self.inventory:
+            self.inventory[item] = quantity
+
+        else:
+            self.inventory[item] = quantity
+
---
+++ Management.py
@@ -0,0 +1,30 @@
+class Management:
+
+    def __init__(self):
+        self.managers = []
+        self.storageSpaces = {}
+
+    def addManagers(self, manager):
+        self.managers.append(manager)
+
+    def getManagers(self):
+        self.managers = []
+
+    def removeManager(self, manager):
+        for i in range(len(self.managers)):
+            if self.managers[i]==manager:
+                self.managers.remove(manager)
+
+    def getStorageSpace(self):
+        return self.storageSpaces
+
+    def removeStorageSpace(self, storageName):
+        if storageName in self.storageSpaces:
+            self.storageSpaces.pop(storageName)
+
+    def getStorageSpace(self):
+        return self.storageSpaces
+
+    def addStorage(self, storage):
+        self.storageSpaces[storage]=[]
+
