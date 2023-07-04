2948608e2ba93846260c98533b8ad7fbd102b9e4
Michael <44344017+mtangtran@users.noreply.github.com>
2023-07-04 19:08:12 -0400

    CRUD principles added to OrderScheduler.py

--- OrderScheduler.py
+++ OrderScheduler.py
@@ -1,4 +1,24 @@
 class OrderScheduler:
 
     def __init__(self):
-        pass
+        self.orders = {}
+        self.NumberedOrdered=0
+    def getOrders(self):
+        return self.NumberedOrdered
+
+    def addOrders(self, order, quantity):
+        self.NumberedOrdered +=1
+        self.orders[order] = quantity
+
+    def deleteOrder(self, order):
+        if order in self.orders:
+            self.orders.pop(order)
+
+    def updateOrderQuantity(self, order, quantity):
+        self.orders[order]=quantity
+
+    def getAllOrders(self):
+        return self.orders
+
+    def deleteAllOrders(self):
+        self.orders = {}
