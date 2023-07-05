c5be77c1c629d9cb742097bf324560980cebf2fa
Michael <44344017+mtangtran@users.noreply.github.com>
2023-07-05 07:13:10 -0400

    Requirements added to InventoryPrediction.py

--- InventoryPrediction.py
+++ InventoryPrediction.py
@@ -3,7 +3,7 @@ class InventoryPrediction:
         self.predictions = []
         self.past = []
         self.totalItems = 0
-
+        self.requirements = []
     def createPrediction(self):
         self.predictions = []
     def getPrediction(self):
@@ -11,3 +11,25 @@ class InventoryPrediction:
 
     def getTotalItems(self):
         return self.totalItems
+
+    def setRequirements(self, date, item, quantity):
+        self.requirements.append((date, item, quantity))
+
+    def getAllRequirements(self):
+        return self.requirements
+
+    def getSpecificRequirement(self, item, date):
+        reqList=[]
+        for i in range(len(self.requirements)):
+            if self.requirements[i][0] == date and self.requirements[i][1] == item:
+                reqList.append(self.requirements[i])
+
+        return reqList
+
+    def clearRequirements(self):
+        self.requirements = []
+
+    def updateRequirements(self, item, date, quantity):
+        for i in range(len(self.requirements)):
+            if self.requirements[i][0] == date and self.requirements[i][1] == item and self.requirements[i][2] == quantity:
+                self.requirements[i] = (item, date, quantity)
